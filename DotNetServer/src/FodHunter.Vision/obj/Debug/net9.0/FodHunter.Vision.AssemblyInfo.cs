8be77bbbc2a298d0ba279a9ed0fa06419a1b0ca7
Eli Manning <eli.manning@calbaptist.edu>
2026-02-06 12:25:10 -0800

    cache update

--- DotNetServer/src/FodHunter.Vision/obj/Debug/net9.0/FodHunter.Vision.AssemblyInfo.cs
+++ DotNetServer/src/FodHunter.Vision/obj/Debug/net9.0/FodHunter.Vision.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FodHunter.Vision")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8d23fde26f15f95e3273b0f3ee5938119573a9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c1ee25c0a69e14e8fee7635a3f83a3067aebfea")]
 [assembly: System.Reflection.AssemblyProductAttribute("FodHunter.Vision")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FodHunter.Vision")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
